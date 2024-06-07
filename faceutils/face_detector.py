a73b5b6b29fcd53998bb7ca6bdf13e3bbf8a4d51
SrikarTA <147472008+SrikarTA@users.noreply.github.com>
2024-06-07 13:35:01 +0530

    Update face_detector.py

--- faceutils/face_detector.py
+++ faceutils/face_detector.py
@@ -7,7 +7,6 @@ _base_dir = os.path.dirname(__file__)
 
 _detector = dlib.get_frontal_face_detector()
 _predictor = dlib.shape_predictor("/kaggle/input/modelfiles/shape_predictor_68_face_landmarks.dat")
-)
 _recognizer = dlib.face_recognition_model_v1(
     os.path.join(os.path.join(_base_dir, "data"),
                  "dlib_face_recognition_resnet_model_v1.dat")
